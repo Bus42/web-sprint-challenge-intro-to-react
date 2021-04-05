47e8478f0a9005f3528b9a056cf15b958f320e7b
Greg Brewton <gbrewton1980@gmail.com>
2021-04-04 20:32:27 -0600

    Fetches data, stores in state, and renders data

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,25 @@
-// Write your Character component here
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+// import styled from 'styled-components';
+
+const Characters = () => {
+    const [characters, setCharacters] = useState([])
+    useEffect(() => {
+        axios.get('http://swapi.dev/api/people/')
+        .then(res => res.data)
+        .then(data => setCharacters(data.results))
+        .catch(err => console.error(err))
+    }, [])
+    return (
+        characters
+        ? <div id="characters_wrapper">
+            {characters.map((person, index) => <div className="person" key={index}>
+                <h2>{person.name}</h2>
+                <p>Born in {person.birth_year}, {person.name} is {person.height}cm tall with {person.eye_color} eyes and weighs {person.mass} kilos. </p>
+            </div> )}
+        </div>
+        : <div className="loader"></div>
+    )
+}
+
+export default Characters;
